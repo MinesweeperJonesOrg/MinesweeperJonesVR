4e771d616d3ab02823d9fc03e1d1c23f946d9c76
Doggeti <dominik.gerl@gmail.com>
2016-10-03 13:15:59 +0100

    Sway Script improved
    Now only the platform the player is standing on should sway and others
    should go back to their default position

--- Assets/EigeneAssets/Sway.cs
+++ Assets/EigeneAssets/Sway.cs
@@ -3,56 +3,62 @@ using UnityEngine;
 
 public class Sway : MonoBehaviour
 {
-	public float Max = 0.1f;
-	public float Speed = 1;
+    public float Max = 0.1f;
+    public float Speed = 1;
 
-	public Transform HMD;
+    public Transform HMD;
 
-	// Use this for initialization
-	void Start()
-	{
+    // Use this for initialization
+    void Start()
+    {
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-		if (HMD == null)
-		{
-			SteamVR_TrackedObject[] trackedObjects = FindObjectsOfType<SteamVR_TrackedObject>();
+    // Update is called once per frame
+    void Update()
+    {
+        if (HMD == null)
+        {
+            SteamVR_TrackedObject[] trackedObjects = FindObjectsOfType<SteamVR_TrackedObject>();
 
-			foreach (SteamVR_TrackedObject tracked in trackedObjects)
-			{
-				if (tracked.index == SteamVR_TrackedObject.EIndex.Hmd)
-				{
-					HMD = tracked.transform;
-					break;
-				}
-			}
-		}
+            foreach (SteamVR_TrackedObject tracked in trackedObjects)
+            {
+                if (tracked.index == SteamVR_TrackedObject.EIndex.Hmd)
+                {
+                    HMD = tracked.transform;
+                    break;
+                }
+            }
+        }
 
-		if (HMD != null)
-		{
-			Debug.Log ("Found HMD");
+        if (HMD != null)
+        {
+            float x = HMD.position.x;
+            float z = HMD.position.z;
 
-			float x = HMD.position.x;
-			float z = HMD.position.z;
+            float xDiff = x - transform.position.x;
+            float zDiff = z - transform.position.z;
 
-			if (Math.Abs(x) > Max)
-			{
-				x = Math.Sign(x) * Max;
-			}
+            Quaternion target = new Quaternion(0, 0, 0, 1);
 
-			if (Math.Abs(z) > Max)
-			{
-				z = Math.Sign(z) * Max;
-			}
+            if (Math.Abs(xDiff) <= 1 && Math.Abs(zDiff) <= 1)
+            {
+                if (Math.Abs(xDiff) > Max)
+                {
+                    xDiff = Math.Sign(xDiff) * Max;
+                }
 
-			Quaternion current = transform.rotation;
+                if (Math.Abs(zDiff) > Max)
+                {
+                    zDiff = Math.Sign(zDiff) * Max;
+                }
 
-			Quaternion target = new Quaternion(z, 0, -x, 1);
+                target = new Quaternion(zDiff, 0, -xDiff, 1);
+            }
 
-			transform.rotation = Quaternion.Slerp(current, target, Speed * Time.deltaTime);
-		}
-	}
+            Quaternion current = transform.rotation;
+
+            transform.rotation = Quaternion.Slerp(current, target, Speed * Time.deltaTime);
+        }
+    }
 }
